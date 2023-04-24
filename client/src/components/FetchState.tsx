fca1968f33a1975db54bfe2e835d1df8b02985ec
navyjeongs <wjdtjr8649@naver.com>
2023-04-24 16:31:10 +0900

    feat: add component
    공통으로 사용할 컴포넌트 추가

---
+++ client/src/components/FetchState.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Loading = styled.div`
+  display: flex;
+  height: 5rem;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10rem;
+  font-size: 2rem;
+`;
+
+export const Error = styled.div`
+  display: flex;
+  height: 5rem;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10rem;
+  font-size: 2rem;
+`;

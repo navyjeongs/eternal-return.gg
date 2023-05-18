f26d7b3383f6ff6114090cea550a57e829fea924
navyjeongs <wjdtjr8649@naver.com>
2023-05-19 00:45:20 +0900

    refactor: 음식 생성기 컴포넌트 반응형 적용 및 타입스크립트로 마이그레이션

--- client/src/components/Food/FoodPolygon.tsx
+++ client/src/components/Food/FoodPolygon.tsx
@@ -39,97 +39,97 @@ const Poly1 = styled.polygon.attrs(() => ({
 `;
 
 // 연못 Polygon
-const Poly2 = styled(Poly1).attrs(() => ({
+const Poly2 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "289,244 329,210 323,206 364,171 319,132 311,138 298,128 269,152 263,147 246,160 249,163 220,188 227,194 228,192 240,201 236,206 254,222 269,227",
   id: "2",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 모래사장 Polygon
-const Poly3 = styled(Poly1).attrs(() => ({
+const Poly3 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "71,292 117,252 87,225 79,232 65,219 61,222 54,216 48,220 28,202 26,203 25,205 26,207 25,210 24,210 20,211 17,219 20,227 24,228 24,232 22,236 24,236 23,238 31,253 34,256 32,259 32,264 39,268 42,267 42,271 44,276 51,276 54,272 66,283 65,287",
   id: "3",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 고급 주택가 Polygon
-const Poly4 = styled(Poly1).attrs(() => ({
+const Poly4 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "72,292 85,303 76,311 80,313 78,315 86,321 81,325 94,336 99,331 111,341 116,337 125,345 142,354 194,310 189,304 168,296 163,292 118,252",
   id: "4",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 골목길 Polygon
-const Poly5 = styled(Poly1).attrs(() => ({
+const Poly5 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "114,57 175,110 238,58 249,68 252,67 269,81 281,72 273,64 283,54 236,14 226,20 206,2 193,14 177,0 124,46 125,47",
   id: "5",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 호텔 Polygon
-const Poly6 = styled(Poly1).attrs(() => ({
+const Poly6 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "52,123 68,137 71,133 131,185 79,231 64,219 61,222 54,216 48,220 24,199 27,197 3,176 6,174 1,169 30,145 28,143",
   id: "6",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 번화가 Polygon
-const Poly7 = styled(Poly1).attrs(() => ({
+const Poly7 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "175,111 196,129 159,162 174,176 181,169 180,168 190,159 200,167 198,169 208,177 211,177 218,182 217,185 220,188 249,163 247,161 263,147 269,152 298,127 285,116 296,106 252,67 250,68 238,58",
   id: "7",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 병원 Polygon
-const Poly8 = styled(Poly1).attrs(() => ({
+const Poly8 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points: "364,171 371,176 360,185 400,219 343,269 336,264 328,271 294,241 330,211 323,206 338,194",
   id: "8",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 절 Polygon
-const Poly9 = styled(Poly1).attrs(() => ({
+const Poly9 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "356,163 392,132 374,118 380,113 332,72 325,79 312,67 300,77 289,68 283,73 282,72 270,83 296,105 285,115 311,138 319,132",
   id: "9",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 양궁장 Polygon
-const Poly10 = styled(Poly1).attrs(() => ({
+const Poly10 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "87,146 125,113 115,105 141,82 114,58 111,60 115,63 95,81 84,71 68,84 71,86 53,102 62,110 61,114 52,123 67,136 71,133",
   id: "10",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 묘지 Polygon
-const Poly11 = styled(Poly1).attrs(() => ({
+const Poly11 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "287,307 327,271 294,242 289,245 269,227 255,223 236,206 229,211 228,209 218,218 219,221 212,227 208,226 199,235 201,236 194,242 242,286 253,278",
   id: "11",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 숲 Polygon
-const Poly12 = styled(Poly1).attrs(() => ({
+const Poly12 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points:
     "207,254 164,292 87,225 158,162 174,176 172,178 169,178 162,182 162,185 153,194 151,193 141,202 150,211 152,210 162,218 162,222 169,227 172,227 181,234 179,236 190,245 193,243",
   id: "12",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 공장 Polygon
-const Poly13 = styled(Poly1).attrs(() => ({
+const Poly13 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points: "336,264 357,282 354,285 391,317 358,346 348,339 320,363 316,360 306,368 283,349 279,352 258,333",
   id: "13",
-}))<{ isClick: boolean }>``;
+}))``;
 
 // 성당 Polygon
-const Poly14 = styled(Poly1).attrs(() => ({
+const Poly14 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points: "207,254 243,285 253,277 287,307 239,347 189,304 168,296 164,292",
   id: "14",
-}))<{ isClick: boolean }>``;
+}))``;
 
-const Poly15 = styled(Poly1).attrs(() => ({
+const Poly15 = styled(Poly1).attrs<typeof Poly1>(() => ({
   points: "141,82 196,130 132,185 87,146 125,113 115,104",
   id: "15",
-}))<{ isClick: boolean }>``;
+}))``;
 
 interface FoodProps {
   area: Array<AreaMaterial>;
--- client/src/components/Food/FoodRouteMain.tsx
+++ client/src/components/Food/FoodRouteMain.tsx
@@ -15,10 +15,16 @@ interface MakeItemImgProps {
   grade: "common" | "epic" | "legend" | "rare" | "uncommon";
 }
 
+// 라우트에서 음식 재료 가져올 때
+export interface RoutePath {
+  path: number;
+  isClick: boolean;
+}
+
 const Container = styled.div`
   width: 100%;
   max-width: 108rem;
-  margin: 0 auto;
+  margin: 2rem auto;
 `;
 
 const ImgContainer = styled.div`
@@ -31,11 +37,6 @@ const ImgContainer = styled.div`
   background-size: 100%;
 `;
 
-const BackSvg = styled.svg`
-  background-image: url("/img/lumia/lumia3.png");
-  background-size: 400px 400px;
-`;
-
 const Wrapper = styled.div`
   display: flex;
   margin: 0 auto;
@@ -45,14 +46,32 @@ const Wrapper = styled.div`
   }
 `;
 
-const FoodDiv = styled.div``;
+const FoodContainer = styled.div``;
+const FoodTitle = styled.h2`
+  font-size: 2rem;
+`;
 
-const SelectArea = styled.div``;
+const ResetBtn = styled.button`
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0.2rem solid var(--color__txt);
+  padding: 0;
 
-// 스태미너 div 전체를 포함하는 div
+  cursor: pointer;
+  border-radius: 1rem;
+`;
+
 const MakeFood = styled.div`
   display: flex;
   column-gap: 2rem;
+
+  @media screen and (max-width: 1080px) {
+    flex-direction: column;
+    row-gap: 2rem;
+  }
 `;
 
 const MakeItem = styled.div`
@@ -119,6 +138,10 @@ const MakeItemMaterialImg = styled(MainItemImg).attrs<MakeItemImgProps>((prop) =
 `;
 
 const FoodRouteMain = () => {
+  // 각 지역에 있는 item과 해당 지역 선택 여부
+  const [area, setArea] = useState<Array<AreaMaterial>>(areaMaterialItem);
+
+  // 현재 가지고 있는 재료 아이템
   const [haveMaterial, setHaveMaterial] = useState<Map<string, number>>(
     new Map([
       ["빵", 1],
@@ -126,34 +149,25 @@ const FoodRouteMain = () => {
     ])
   );
 
+  // 만들 수 있는 체력 아이템
   const [canMakeHPItem, setCanMakeHpItem] = useState<Array<HPFood>>([]);
+  // 만들 수 있는 스태미너 아이템
   const [canMakeMPItem, setCanMakeMpItem] = useState<Array<MPFood>>([]);
-  /// 위에 가 새로 추가
 
-  // 입력한 루트의 title
-  const [routeTitle, setRouteTitle] = useState("");
+  // 만약 루트를 가져왔다면 가쟈온 루트의 제목
+  const [routeTitle, setRouteTitle] = useState<string>("");
 
-  // 각 지역에 있는 item과 해당 지역 선택 여부
-  const [area, setArea] = useState<Array<AreaMaterial>>(areaMaterialItem);
+  /**
+   * 선택지역을 확인해서 가지고 있는 재료를 변경
+   */
+  const handleHaveMaterial = () => {
+    let tmpMap = new Map<string, number>([]);
 
-  // 가지고 있는 재료 변경
-  const handleHaveMaterial = (id: number, isClick: boolean): void => {
-    let tmpMap = new Map(haveMaterial);
-
-    const { item } = areaMaterialItem[id];
-
-    // 지역을 선택한거면
-    if (isClick) {
-      for (let i = 0; i < item.length; i++) {
-        tmpMap.set(item[i], (tmpMap.get(item[i]) || 0) + 1);
-      }
-    }
-    // 선택 해제 한거면
-    else {
-      for (let i = 0; i < item.length; i++) {
-        tmpMap.set(item[i], (tmpMap.get(item[i]) || 1) - 1);
-        if (tmpMap.get(item[i]) === 0) {
-          tmpMap.delete(item[i]);
+    for (let i = 0; i < area.length; i++) {
+      const { item } = areaMaterialItem[i];
+      if (area[i].isClick) {
+        for (let j = 0; j < item.length; j++) {
+          tmpMap.set(item[j], (tmpMap.get(item[j]) || 0) + 1);
         }
       }
     }
@@ -161,7 +175,9 @@ const FoodRouteMain = () => {
     setHaveMaterial(tmpMap);
   };
 
-  // 음식 만들기
+  /**
+   * 가지고 있는 재료로 음식을 만듦
+   */
   const handleMakeFood = () => {
     let possibleMakeHpItem: Set<HPFood> = new Set();
     let possibleMakeMpItem: Set<MPFood> = new Set();
@@ -190,34 +206,38 @@ const FoodRouteMain = () => {
     setCanMakeMpItem(Array.from(possibleMakeMpItem));
   };
 
-  // 지역 선택시 실행되는 함수
+  /**
+   * 지역 선택시 실행되는 함수
+   * @param e
+   */
   const isClickArea = async (e: React.SyntheticEvent<SVGElement>) => {
     const { id } = e.target as SVGPolygonElement;
 
-    let numberId: number = +id; // id가 string이므로 배열에서 접근할 때 number가 필요하므로 number로 변환
+    // 정확히 지역만 클릭했을 때
+    if (id !== "") {
+      let numberId: number = +id; // id가 string이므로 배열에서 접근할 때 number가 필요하므로 number로 변환
 
-    // 현재 false라면 만들 수 있게 변경
-    handleHaveMaterial(numberId, !area[numberId].isClick);
-
-    setArea(
-      produce((draft) => {
-        draft[numberId].isClick = !draft[numberId].isClick;
-      })
-    );
-  };
-
-  // 클릭 한 지역명만 가져오기
-  const clickAreaTrue = () => {
-    let clickArea = " ";
-
-    for (let i = 0; i < area.length; i++) {
-      if (area[i].isClick === true) {
-        clickArea += " " + area[i].name + " ";
-      }
+      // 현재 false라면 만들 수 있게 변경
+      setArea(
+        produce((draft) => {
+          draft[numberId].isClick = !draft[numberId].isClick;
+        })
+      );
     }
-    return <span>{clickArea}</span>;
   };
 
+  /**
+   * 폴리건을 클릭하거나
+   * 리셋버튼을 누르거나
+   * 루트ID를 검색하면 실행
+   */
+  useEffect(() => {
+    handleHaveMaterial();
+  }, [area]);
+
+  /**
+   * 가지고 있는 재료가 변경되면 음식 handleMakeFood를 실행하여 음식 만들기
+   */
   useEffect(() => {
     handleMakeFood();
   }, [haveMaterial]);
@@ -227,23 +247,11 @@ const FoodRouteMain = () => {
       <Helmet>
         <title>이터널리턴 전적검색 - 음식루트 생성기</title>
       </Helmet>
-
-      <FoodRouteHeader area={area} setArea={setArea} setRouteTitle={setRouteTitle} />
-
+      <FoodRouteHeader setArea={setArea} setRouteTitle={setRouteTitle} />
       <Wrapper>
         <FoodPolygon area={area} isClickArea={isClickArea} />
-        <FoodDiv>
-          <h3>루트 제목 : {routeTitle}</h3>
-          <SelectArea>선택한 지역 : {clickAreaTrue()}</SelectArea>
-          <svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14.25 14.375L8.75 19.5417L14.25 24.7083" stroke="var(--color__txt)" stroke-width="2" />
-            <path
-              d="M3.16451 15.0208C2.10511 13.2971 1.68079 11.2932 1.95734 9.31981C2.2339 7.34645 3.19589 5.51397 4.6941 4.10656C6.19231 2.69914 8.14302 1.79546 10.2437 1.53567C12.3444 1.27587 14.4776 1.67448 16.3125 2.66967C18.1474 3.66486 19.5815 5.20101 20.3923 7.03988C21.2032 8.87876 21.3454 10.9176 20.797 12.8401C20.2487 14.7627 19.0403 16.4615 17.3593 17.6732C15.6784 18.8849 13.6188 19.5416 11.5 19.5416"
-              stroke="var(--color__txt)"
-              stroke-width="2"
-              stroke-linecap="round"
-            />
-          </svg>
+        <FoodContainer>
+          <FoodTitle>루트 제목 : {routeTitle}</FoodTitle>
           <MakeFood>
             <MakeItem>
               <MakeItemHeaderWrapper>
@@ -308,7 +316,7 @@ const FoodRouteMain = () => {
               })}
             </MakeItem>
           </MakeFood>
-        </FoodDiv>
+        </FoodContainer>
       </Wrapper>
     </Container>
   );
